cf69c26317ede560fcf36fc308bc55817b99232f
s1260217 <s1260217@u-aizu.ac.jp>
2018-05-21 18:39:28 +0900

    add solution ITP1_2_C.2

--- ITP1_2/ITP1_2_C.c
+++ ITP1_2/ITP1_2_C.c
@@ -1,20 +1,38 @@
 #include<stdio.h>
 int main(){
- int a,b,c;
- scanf("%d %d %d",&a,&b,&c);
- if(a < b && b < c){
- printf("%d %d %d\n",a,b,c);
- }else if(a < c && c < b){
- printf("%d %d %d\n",a,c,b);
- }else if(b < a && a < c){
- printf("%d %d %d\n",b,a,c);
- }else if(b < c && c < a ){
- printf("%d %d %d\n",b,c,a);
- }else if(c < a && a < b ){
- printf("%d %d %d\n",c,a,b);
- }else if(c < b && b < a ){
- printf("%d %d %d\n",c,b,a);
- }
-   return 0;
+  int a,b,c;
+  scanf("%d %d %d",&a,&b,&c);
+  /*
+    if(a < b && b < c){
+    printf("%d %d %d\n",a,b,c);
+    }else if(a < c && c < b){
+    printf("%d %d %d\n",a,c,b);
+    }else if(b < a && a < c){
+    printf("%d %d %d\n",b,a,c);
+    }else if(b < c && c < a ){
+    printf("%d %d %d\n",b,c,a);
+    }else if(c < a && a < b ){
+    printf("%d %d %d\n",c,a,b);
+    }else if(c < b && b < a ){
+    printf("%d %d %d\n",c,b,a);
+    }
+  */
+  if (a > b) {
+    int t = a;
+    a = b;
+    b = t;
+    printf("%d %d %d\n",a,b,c);
+  }else if(b > c){
+    int t = b;
+      b = c;
+      c = t;
+      printf("%d %d %d\n",a,b,c);
+  }else if(a > c){
+    int t = a;
+    a = c;
+    c = t;
+    printf("%d %d %d\n",a,b,c);
+  }
+  return 0;
 }
  
