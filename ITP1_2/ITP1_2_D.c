64a6778f0b2b589b3e3c4099d7e862eb49266c54
s1260217 <s1260217@u-aizu.ac.jp>
2018-05-23 15:09:01 +0900

    add solution for ITP1_2_D.c

--- ITP1_2/ITP1_2_D.c
+++ ITP1_2/ITP1_2_D.c
@@ -3,15 +3,12 @@
 int main(){
   int W,H,x,y,r;
   scanf("%d %d %d %d %d",&W,&H,&x,&y,&r);
-  if(x-r < 0 || x+r > W){
-    printf("No\n");
-  }else if(x-r >= 0 && x+r <= W){
-      printf("Yes\n");
-  }
-  if(y-r < 0 || y+r > H){
-    printf("NO\n");
-  }else if(y-r >= 0 && y-r <= H){
+
+  if(x-r >= 0 && x+r <= W && y-r >=0 && y+r <= H){
     printf("Yes\n");
+  }
+  else{
+    printf("No\n");
   }
   return 0;
     }
