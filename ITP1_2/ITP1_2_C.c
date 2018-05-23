3d21817060a5a38adffce61fece4bc8a359b9e20
s1260217 <s1260217@u-aizu.ac.jp>
2018-05-23 14:42:33 +0900

    add solution for ITP1_2_C.c

--- ITP1_2/ITP1_2_C.c
+++ ITP1_2/ITP1_2_C.c
@@ -1,33 +1,23 @@
 #include<stdio.h>
 int main(){
   int a,b,c;
+  int temp;
+
   scanf("%d %d %d",&a,&b,&c);
-  /*
-    if(a < b && b < c){
-    printf("%d %d %d\n",a,b,c);
-    }else if(a < c && c < b){
-    printf("%d %d %d\n",a,c,b);
-    }else if(b < a && a < c){
-    printf("%d %d %d\n",b,a,c);
-    }else if(b < c && c < a ){
-    printf("%d %d %d\n",b,c,a);
-    }else if(c < a && a < b ){
-    printf("%d %d %d\n",c,a,b);
-    }else if(c < b && b < a ){
-    printf("%d %d %d\n",c,b,a);
-    }
-  */
-  if (a > b) {
-    int t = a;
+
+  if(a > b){
+    temp = a;
     a = b;
-    b = t;
-    printf("%d %d %d\n",a,b,c);
-  }else if(b > c){
-    int t = b;
-      b = c;
-      c = t;
-      printf("%d %d %d\n",a,b,c);
+    b = temp;
   }
+
+  if(b > c){
+    temp = b;
+    b = c;
+    c = temp;
+  }
+  printf("%d %d %d\n",a,b,c);
+
   return 0;
 }
  
