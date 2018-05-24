ec33cceda6997402f30e73312b31a87bbd3b723a
s1260217 <s1260217@u-aizu.ac.jp>
2018-05-24 17:33:19 +0900

    add solution for ITP1_5_A.c

--- ITP1_5/ITP1_5_A.c
+++ ITP1_5/ITP1_5_A.c
@@ -4,14 +4,16 @@ int main(){
   int i;
   int H;
   int W;
-  scanf("%d %d",&H,&W);
-  
-  for(i = 0; i < H; i++){
-    if (H == 0 && W == 0) break;
-  for(j = 0; j < W; j++ ){
-    printf("#");
+
+  while(1){
+      scanf("%d %d",&H,&W);
+    if(H == 0 && W == 0) break;
+    for(i = 0; i < H; i++){
+      for(j = 0; j < W; j++){
+	printf("#");
+      }
+      printf("\n");
+    }
   }
-  printf("\n");
-}
   return 0;
 }
---
+++ ITP1_5/ITP1_5_A.c~
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+  int j;
+  int i;
+  int H;
+  int W;
+  scanf("%d %d",&H,&W);
+  
+  for(i = 0; i < H; i++){
+    if (H == 0 && W == 0) break;
+  for(j = 0; j < W; j++ ){
+    printf("#");
+  }
+  printf("\n");
+}
+  return 0;
+}
