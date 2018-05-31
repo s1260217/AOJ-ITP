ead21939001e694a2404e134d67afbcbc7fafb7f
s1260217 <s1260217@u-aizu.ac.jp>
2018-05-31 17:19:57 +0900

    add solution for ITP1_5_B.c

--- ITP1_5/ITP1_5_B.c
+++ ITP1_5/ITP1_5_B.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main(){
-  int i,j,H,W;
   while(1){
-    if(H == 0 && W == 0) break;
-    scanf("%d %d",&H,&W);
-  for(i = 0; i < H; i++){
+    int i,j,H,W;
+  scanf("%d %d ",&H,&W);
+  if(H == 0 && W == 0) break;
+  for(i = 0; i <= H; i++){
     for(j = 0; j < W; j++){
-      if(i == 0 || i ==(H-1) || j == 0 || j ==(W-1)){
-            printf("#");
-        } else {
-            printf(".");
-        }
+      if(i == 1 || i ==H || j == 1 || j == W){
+	printf("#");
+      }else {
+	printf(".");
+	  }
     }
     printf("\n");
   }
-  printf("\n");
-}
   return 0;
+  }
 }
+  
