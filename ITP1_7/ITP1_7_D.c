8e2d6cdc6f7a3dc6d4f55d9482e6df7687cc0a2a
s1260217 <s1260217@u-aizu.ac.jp>
2018-07-12 17:30:19 +0900

    add solution for ITP1_7_D

---
+++ ITP1_7/ITP1_7_D.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+  int n,m,l;
+  int i,j,k;
+  int a[101][101];
+  int b[101][101];
+  int c[101][101];
+  scanf("%d %d %d",&n,&m,&l);
+  for(i = 0; i < n; i++){
+    for(j = 0; j < m; j++){
+      scanf("%d",&a[i][j]);
+    }
+  }
+  for(i = 0; i < m; i++){
+    for(j = 0; j < l; j++){
+      scanf("%d",&b[i][j]);
+    }
+  }
+
+  for(i = 0; i < n; i++){
+    for(j = 0; j < l; j++){
+      c[i][j] = 0;
+      for(k=0; k < m; k++){
+      c[i][j] += a[i][k] * b[k][j];
+      }
+         printf("%d ",c[i][j]);
+      }
+    printf("\n");
+  }
+  
+  return 0;
+}
